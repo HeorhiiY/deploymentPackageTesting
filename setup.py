f2858cd53304766ccf69c3affc91f7f779491711
Heorhii Yehiazarian <heorhii.yehiazarian@nextnano.com>
2022-04-11 16:20:54 +0200

    release 1.0.2

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='deploymentPackageTesting',
-    version='1.0.1',
+    version='1.0.2',
     author='Heorhii Yehiazarian',
     author_email='heorhii.yehiazarian@nextnano.com',
     description='simple package for testing purposes',
-    packages=find_packages(exclude=["test"])
+    packages=find_packages(exclude=["test"]),
+    install_requires = ['numpy>=1.18']
 )
