cf9b58c59c52ab203abeaefe8048ff3324e80c61
Heorhii Yehiazarian <heorhii.yehiazarian@nextnano.com>
2022-04-11 16:16:19 +0200

    added second function

--- test/function_test.py
+++ test/function_test.py
@@ -1,9 +1,15 @@
 import unittest
-from mymodule.functions import say_hi
+import numpy as np
+from mymodule.functions import say_hi, make_np_array
 class TestFunction(unittest.TestCase):
     def test_say_hi(self):
         name = 'Brandon'
         expected_result = 'Hi, Brandon!'
         self.assertEqual(expected_result, say_hi(name))
+
+    def test_make_np_array(self):
+        ar = [3,3,3]
+        expected_result = np.array(ar)
+        self.assertTrue(np.alltrue(expected_result == make_np_array(ar)))
 
 
