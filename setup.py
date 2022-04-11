aa489d9897b00136e2ae41c04fadd05334203e16
HeorhiiY <59930103+HeorhiiY@users.noreply.github.com>
2022-04-11 16:04:09 +0200

    version 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='deploymentPackageTesting',
-    version='1.0.0',
+    version='1.0.1',
     author='Heorhii Yehiazarian',
     author_email='heorhii.yehiazarian@nextnano.com',
     description='simple package for testing purposes',
